039b4ad41e1e91cdc55a10a20f5d58f5011822b5
Prasad <varaprasad1819@gmail.com>
2023-08-01 18:44:56 +0530

    registration  test case

---
+++ src/test/java/com/pageObjects/RegistrationPage.java
@@ -0,0 +1,46 @@
+package com.pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.base.Base;
+
+public class RegistrationPage extends Base{
+	
+	public RegistrationPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(id = "firstname")
+	WebElement firstName;
+	
+	@FindBy(id = "lastname")
+	WebElement lastName;
+	
+	@FindBy(id = "email_address")
+	WebElement email;
+	
+	@FindBy(id = "password")
+	WebElement password;
+	
+	@FindBy(id = "password-confirmation")
+	WebElement confirmPassword;
+	
+	@FindBy(xpath = "//*[@id=\"form-validate\"]/div/div[1]/button")
+	WebElement submitbutton;
+
+
+	public void newUserRegistration(String firstname, String lastname, String useremail, String userpassword, String confirmpassword) {
+		firstName.sendKeys(firstname);
+		lastName.sendKeys(lastname);
+		email.sendKeys(useremail);
+		password.sendKeys(userpassword);
+		confirmPassword.sendKeys(confirmpassword);
+		submitbutton.click();
+	}
+
+}
+
--- src/test/java/com/testScripts/CreateAccount.java
+++ src/test/java/com/testScripts/CreateAccount.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import com.aventstack.extentreports.Status;
 import com.base.Base;
 import com.pageObjects.HomePage;
+import com.pageObjects.RegistrationPage;
 
 public class CreateAccount extends Base {
 
@@ -12,12 +13,19 @@ public class CreateAccount extends Base {
 	public void createAccount() throws InterruptedException {
 		
 		// Create test for reports
-		test = extent.createTest("Opening of registration Page");
+		test = extent.createTest("Registration of new user");
 		
 		HomePage hp = new HomePage(driver);
 		hp.clikCreateAccount();
 
-		test.log(Status.PASS, "Test passed successfully.");
+		if (driver.getTitle().equalsIgnoreCase("create new Customer account")) {
+			test.log(Status.PASS, "Registration page opened");
+		}
+		
+		RegistrationPage rp = new RegistrationPage(driver);
+		rp.newUserRegistration(fname, lname, useremail, userpassword, confirpassword);
+		
+		span[class="logged-in"]
 		
 	}
 }
