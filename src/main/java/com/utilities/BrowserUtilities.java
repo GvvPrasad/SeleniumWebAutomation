9d7636a8f5f03ea75186c29fba7bdf1f04921368
Prasad <varaprasad1819@gmail.com>
2023-08-20 18:39:09 +0530

    Browser alerts added

--- src/main/java/com/utilities/BrowserUtilities.java
+++ src/main/java/com/utilities/BrowserUtilities.java
@@ -1,5 +1,26 @@
 package com.utilities;
 
-public class BrowserUtilities {
+import com.base.Base;
 
+public class BrowserUtilities extends Base {
+	
+	//Browser alerts
+	public static String browserAlerts(String alerttype, String message) {
+		
+		String alerttext = null;
+		
+		switch (alerttype) {
+		case "accept": driver.switchTo().alert().accept();
+			break;
+		case "dismiss": driver.switchTo().alert().dismiss();
+			break;
+		case "gettext": alerttext = driver.switchTo().alert().getText();
+			break;
+		case "sendtext": driver.switchTo().alert().sendKeys(message);
+			break;
+		}
+		return alerttext;
+	}
+	
+	
 }
