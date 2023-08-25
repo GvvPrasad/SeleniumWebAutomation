5c27cc81b0ba1cc35e732f6007f8cf99c6535f6c
Prasad <varaprasad1819@gmail.com>
2023-08-25 17:44:47 +0530

    Broken links added

--- src/main/java/com/links/BrokenLinks.java
+++ src/main/java/com/links/BrokenLinks.java
@@ -1,5 +1,48 @@
 package com.links;
 
-public class BrokenLinks {
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import com.base.Base;
+
+public class BrokenLinks extends Base {
+
+	// variables
+	static HttpURLConnection hul;
+
+	@Test
+	public static void brokenLinksList() throws IOException {
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+
+		// Get all the links
+		for (WebElement link : links) {
+			String linkText = link.getText();
+			String linkUrl = link.getAttribute("href");
+			
+			if (linkUrl == null || linkUrl.isEmpty()) {
+				System.out.println(linkText+" : "+linkUrl + " :URL is either not configured for anchor tag or it is empty");
+				continue;
+			}
+
+			URL url = new URL(linkUrl);
+
+			// Now we will be creating url connection and getting the response code
+			hul = (HttpURLConnection) url.openConnection();
+			hul.setConnectTimeout(5000);
+			hul.connect();
+			
+			if(hul.getResponseCode()>=400)
+            {
+            	System.out.println(linkText+" : "+linkUrl+" is a broken link");
+            } 
+		}
+
+	}
 
 }
